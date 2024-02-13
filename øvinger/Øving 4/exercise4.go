ef7056f54e609b8cf4c6d6c76f7f996e65134bf3
Håvard <h.lundheim@gmail.com>
2024-02-13 22:58:48 +0100

    øving 4 ferdig!

--- øvinger/Øving 4/exercise4.go
+++ øvinger/Øving 4/exercise4.go
@@ -8,13 +8,7 @@ import (
 	"time"
 )
 
-//countingNr = 0. loop som lytter etter primary ved å opprette client. Oppdaterer verdi på countingNr ved svar.
-//forts. Ved timeout så oppretter den en server og tar primary rollen.
-//opprette backup ved å kjøre exec.Command ... exercise4.go
-//loop som utfører jobben til primary
-
 var countingNr int
-var errorNr int
 
 func handleError(err error) {
 	if err != nil {
@@ -26,28 +20,22 @@ func handleError(err error) {
 func main() {
 	fmt.Println("----Backup phase----")
 	countingNr = 0
-	errorNr = 0
 	//create UDP client
-	port := ":17000"
-	backupSocket, err := net.ListenPacket("udp4", port)
+	port := "127.0.0.1:17000"
+	a, err := net.ResolveUDPAddr("udp4", port)
 	handleError(err)
-
+	backupSocket, err := net.ListenUDP("udp4", a)
 	fmt.Println("Client started")
 	for {
-
 		backupSocket.SetReadDeadline(time.Now().Add(3 * time.Second))
 		buffer := make([]byte, 1024)
-		n, addr, err := backupSocket.ReadFrom(buffer)
+		n, _, err := backupSocket.ReadFromUDP(buffer)
 		if err != nil {
 			break
 		}
 		time.Sleep(time.Millisecond * 500)
-		tempString := string(buffer[:n])
-		countingNr, err = strconv.Atoi(tempString)
-		fmt.Println("Errornr: ", errorNr, "countingNr: ", countingNr, "Addr: ", addr)
-
-		handleError(err)
-		//fmt.Printf(int(buffer[0:messageN]))
+		countingNr, err = strconv.Atoi(string(buffer[:n]))
+		fmt.Println("countingNr: ", countingNr)
 	}
 
 	backupSocket.Close()
@@ -57,24 +45,20 @@ func main() {
 	//broadcastIP := "10.100.23.255:17000"
 	//broadcastIP := "127.0.0.255:17000"
 	broadcastIP := "127.0.0.1:17000"
-	primarySocket, err := net.ListenPacket("udp4", port)
 	addr, err := net.ResolveUDPAddr("udp4", broadcastIP)
-	
-	//defer primarySocket.Close()
-	handleError(err)
+	primarySocket, err := net.DialUDP("udp4", nil, addr)
+
 	//Creating backup
-	// DialDUP Write,,, 
 	fmt.Println("... creating new backup")
-	exec.Command("gnome-terminal", "--", "go", "run", "exercise4.go").Run()
+	//exec.Command("gnome-terminal", "--", "go", "run", "exercise4.go").Run()
+	exec.Command("cmd", "/C", "start", "powershell", "go", "run", "exercise4.go").Run()
 	fmt.Println("Resuming counting from ", countingNr)
+
 	for {
-		temp := strconv.Itoa(countingNr)
-		fmt.Println("a")
-		_, err := primarySocket.WriteTo([]byte(temp), addr)
-		fmt.Println("b")
+		_, err := primarySocket.Write([]byte(strconv.Itoa(countingNr)))
+		handleError(err)
 		time.Sleep(1 * time.Second)
 		countingNr += 1
 		fmt.Println(countingNr)
-		handleError(err)
 	}
 }
