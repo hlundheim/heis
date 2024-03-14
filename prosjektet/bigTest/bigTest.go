d15b2bfb5b8e32f3c27aaedba215c42f1c25cc11
Sveinung Myhre <s-myhre@outlook.com>
2024-03-14 12:39:54 +0100

    added switch of direction at first and last floor in bigTest.go

--- prosjektet/bigTest/bigTest.go
+++ prosjektet/bigTest/bigTest.go
@@ -84,11 +84,13 @@ func UpdateButtonLightsAndListsAtStop(floor int, PRCompletions chan [][2]bool) {
 	elev.DRList[floor] = false
 	elevio.SetButtonLamp(elevio.BT_Cab, floor, false)
 	if floor == numFloors-1 {
+		elev.Direction = elevator.ED_Down
 		PRCompletionList[floor][1] = true
 		PRCompletions <- PRCompletionList
 		//elevio.SetButtonLamp(elevio.BT_HallUp, floor, false)
 		//elevio.SetButtonLamp(elevio.BT_HallDown, floor, false)
 	} else if floor == 0 {
+		elev.Direction = elevator.ED_Up
 		PRCompletionList[floor][0] = true
 		PRCompletions <- PRCompletionList
 	} else {
@@ -224,7 +226,7 @@ func CheckForJobsInDirection(PRCompletions chan [][2]bool) {
 	case elevator.ED_Down:
 		//fmt.Println("Inside elevator.ED_down case of checkforjobsindirection function")
 		//fmt.Println("Elevators PRList: ", elev.PRList)
-		os.Stdout.Sync()
+		//os.Stdout.Sync()
 		if DRHere() {
 			go StopAtFloor(elev.Floor, PRCompletions)
 			//Legge inn å sjekke for PR å åpne døra igjen?
@@ -396,7 +398,7 @@ func HandleDefaultJobsWaiting(PRCompletions chan [][2]bool) {
 
 func Initialize(newPRs chan [][2]bool, recievedPRs chan [][2]bool, PRCompletions chan [][2]bool, globalPRs chan [][2]bool, elevState chan elevator.Elevator) {
 
-	elevio.Init("localhost:23001", numFloors)
+	elevio.Init("localhost:15657", numFloors)
 
 	drv_buttons := make(chan elevio.ButtonEvent)
 	drv_floors := make(chan int)
